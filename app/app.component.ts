43fd870a3925385a99a9914b79d2c75921c6d0e4
Max <namax25@gmail.com>
2016-04-08 13:58:40 -0400

    5 min quick start

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by namax on 4/8/16.
+ */
+import {Component} from "angular2/core";
+
+@Component({
+    selector: "my-app",
+    template: "<h1>My First Angular 2 App</h1>"
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by namax on 4/8/16.
+ */
+
+import {bootstrap}    from "angular2/platform/browser";
+import {AppComponent} from "./app.component";
+
+bootstrap(AppComponent);
