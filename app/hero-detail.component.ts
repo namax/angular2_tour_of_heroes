b74d7b7f9590c437488bcdaf2fc148388b53d67e
Max <namax25@gmail.com>
2016-04-10 08:06:35 -0400

    Dashboard routing

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -2,26 +2,33 @@
  * Created by namax on 4/9/16.
  */
 
-import {Component, Input} from "angular2/core";
-import {Hero} from "./hero";
+import {Component, Input, OnInit} from "angular2/core";
+import {RouteParams} from "angular2/router";
 
+import {Hero} from "./hero";
+import {HeroService} from "./hero.service";
 
 @Component({
     selector: "my-hero-detail",
-    template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>
-`
+    templateUrl: "app/hero-detail.component.html",
 })
 
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+    constructor(private _heroService:HeroService,
+                private _routeParams:RouteParams) {
+    }
+
     @Input()
     hero:Hero;
+
+    ngOnInit() {
+        let id = +this._routeParams.get("id");
+        this._heroService.getHero(id)
+            .then(hero => this.hero = hero);
+    }
+
+    goBack() {
+        window.history.back();
+    }
 }
